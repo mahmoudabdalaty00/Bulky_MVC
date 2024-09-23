0f2db25e5f72d7f2c2c5959b6d846cee538b5afe
hamouda <ma5350@fayoum.edu.eg>
2024-09-23 19:22:32 +0300

    added Areas & start in Product Cls

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -29,7 +29,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
 
@@ -41,7 +40,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "defult",
+	pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
